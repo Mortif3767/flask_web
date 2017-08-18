09157f2c759b260d1a1428f31fe280f270cd7e14
mortif3767 <mortif3767@outlook.com>
2017-08-18 14:35:06 +0800

    2.2

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*- 
+
 from flask import Flask
 app=Flask(__name__)      #创建程序实例
 
 @app.route('/')          #路由：处理URL和函数之间的关系
 def index():             #视图函数
     return '<h1>NIHAO!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+	return "<h1>NiHao,%s</h1>"%name
 
 if __name__=='__main__': #启动服务器
     app.run(debug=True)
