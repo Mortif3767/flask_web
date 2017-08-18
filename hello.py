90b4b6e868321e082666e75079dfd77a09beb7ea
mortif3767 <mortif3767@outlook.com>
2017-08-18 14:26:41 +0800

    2.1.1

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app=Flask(__name__)
+app=Flask(__name__)      #创建程序实例
 
-@app.route('/')
-def index():
+@app.route('/')          #路由：处理URL和函数之间的关系
+def index():             #视图函数
     return '<h1>NIHAO!</h1>'
 
-if __name__=='__main__':
+if __name__=='__main__': #启动服务器
     app.run(debug=True)
