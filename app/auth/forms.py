14538b8f33ed2aee639bbf84c7ba74e7d07e634d
mortif3767 <mortif3767@outlook.com>
2017-08-25 17:13:43 +0800

    完整注册功能

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +12,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField(u'邮箱', validators=[Required(), Length(1, 64),
+                                             Email()])
+    username = StringField(u'用户名', validators=[
+    	Required(), Length(1, 64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+    	u'用户名必须是字母、数字、下划线、点号')])
+    password = PasswordField(u'密码', validators=[
+    	Required(), EqualTo('password2', message=u'密码不一致')])
+    password2 = PasswordField(u'确认密码', validators=[Required()])
+    submit = SubmitField(u"注册")
+
+
+    def validate_email(self,field):
+    	if User.query.filter_by(email=field.data).first():
+    		raise ValidationError(u'邮箱已被注册')
+
+    def validate_username(self,field):
+    	if User.query.filter_by(username=field.data).first():
+    		raise ValidationError(u'用户名已存在')
