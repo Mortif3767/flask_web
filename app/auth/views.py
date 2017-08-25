2703e5be8271dbbc2dfde5105cbf802a0f36fb9d
mortif3767 <mortif3767@outlook.com>
2017-08-25 22:09:12 +0800

    完整注册功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
+from ..email import send_email
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -14,24 +15,38 @@ def login():
 		if user is not None and user.verify_password(form.password.data):
 			login_user(user, form.remember_me.data)
 			return redirect(request.args.get('next') or url_for('main.index'))
-		flash('Invalid username or password!')
+		flash(u'错误用户名或密码!')
 	return render_template('auth/login.html',form=form)
 
 @auth.route('/logout')
 @login_required   #only authenticated user are allowed
 def logout():
-	logout_user
+	logout_user()
 	flash(u"你已经退出。")
 	return redirect(url_for('main.index'))
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
 	form = RegistrationForm()
+	token = None
 	if form.validate_on_submit():
 		user = User(email=form.email.data,
 			        username=form.username.data,
 			        password=form.password.data)
 		db.session.add(user)
-		flash(u'现在可以登录')
+		db.session.commit()
+		token = user.generate_confirmation_token()
+		flash(u'注意点击确认链接')
+		
+	return render_template('auth/register.html', form=form, token = token)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+	if current_user.confirmed:
 		return redirect(url_for('main.index'))
-	return render_template('auth/register.html', form=form)
+	if current_user.confirm(token):
+		flash(u'你已经成功激活你的账户')
+	else:
+		flash(u'链接已经失效，请重试')
+	return redirect(url_for('main.index'))
--- app/main/views.py
+++ app/main/views.py
@@ -1,29 +1,7 @@
-# -*- coding: utf-8 -*- 
-from datetime import datetime
-from flask import render_template,session,redirect,url_for
+from flask import render_template
+from . import main
 
-from . import main            #从main／__init__.py中导入蓝本
-from .forms import NameForm   #从main／forms导入NameForm类
-from .. import db             #从app/__init__.py中导入db=SQLAlchemy（）
-from ..models import User     #从app／models导入User数据库表
 
-@main.route('/',methods=['GET','POST'])
+@main.route('/')
 def index():
-	name=None
-	form=NameForm()
-	if form.validate_on_submit():
-		user=User.query.filter_by(username=form.name.data).first()
-		if user is None:
-			user=User(username=form.name.data)
-			db.session.add(user)
-			session['known']=False
-			if app.config['FLASKY_ADMIN']:
-				send_email(app.config['FLASKY_ADMIN'],'New User',
-					       'mail/new_user',user=user)
-		else:
-			session['known']=True
-		session['name']=form.name.data
-		form.name.data=''
-		return redirect(url_for('main.index'))
-	return render_template("index.html",form=form,name=session.get('name'),
-		known=session.get('known',False),current_time=datetime.utcnow())
+    return render_template('index.html')
