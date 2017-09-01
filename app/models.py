e2b2959925e3196a265ff6fbd930f34571ccffbe
mortif3767 <mortif3767@outlook.com>
2017-09-01 14:56:18 +0800

    profile

--- app/models.py
+++ app/models.py
@@ -62,6 +62,7 @@ class User(UserMixin, db.Model):
 	about_me = db.Column(db.Text())
 	member_since = db.Column(db.DateTime(), default=datetime.utcnow)
 	last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+	avatar_hash = db.Column(db.String(32))
 
 	def __init__(self,**kwargs):
 		super(User, self).__init__(**kwargs)
@@ -70,6 +71,9 @@ class User(UserMixin, db.Model):
 				self.role = Role.query.filter_by(permissions=0xff).first()
 			if self.role is None:
 				self.role = Role.query.filter_by(default=True).first()
+		if self.email is not None and self.avatar_hash is None:
+			self.avatar_hash = hashlib.md5(
+				self.email.encode('utf-8')).hexdigest()
 
 	@property
 	def password(self):
@@ -132,6 +136,8 @@ class User(UserMixin, db.Model):
 		if self.query.filter_by(email=new_email).first() is not None:
 			return False
 		self.email = new_email
+		self.avatar_hash = hashlib.md5(
+			self.email.encode('utf-8')).hexdigest()
 		db.session.add(self)
 		return True
 
