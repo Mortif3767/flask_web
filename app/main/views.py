cac399074137506154272e02ccad9c050a8c6528
mortif3767 <mortif3767@outlook.com>
2017-09-12 18:54:17 +0800

    显示关注的文章

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, flash, redirect, url_for, abort, request,\
-    current_app
+    current_app, make_response
 from . import main
 from ..models import User, Role, Permission, Post
 from flask_login import login_required, current_user
@@ -20,11 +20,35 @@ def index():
         return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)     #dict.get(key, default=None, type=None)
     #默认页面是第一页
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+    show_followed = False
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed', ''))   #转换成布尔值
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+    pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
         error_out=False)                             #返回第page页，每页*个记录
     posts = pagination.items                         #留意pagination对象是什么？items是该对象的属性
-    return render_template('index.html', form=form, posts=posts, pagination=pagination)
+    return render_template('index.html', form=form, posts=posts, show_followed=show_followed, 
+        pagination=pagination)
+
+
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('main.index')))
+    resp.set_cookie('show_followed', '', max_age=30*24*60%60)
+    return resp
+
+
+@main.route('/followed_posts')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('main.index')))
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+    return resp
 
 
 @main.route('/user/<username>')
--- app/models.py
+++ app/models.py
@@ -98,6 +98,11 @@ class User(UserMixin, db.Model):
 				self.email.encode('utf-8')).hexdigest()
 
 	@property
+	def followed_posts(self):
+		return Post.query.join(Follow, Follow.followed_id == Post.author_id)\
+		    .filter(Follow.follower_id == self.id)
+
+	@property
 	def password(self):
 		raise AttributeError('密码是不可读属性！')
 
