b128c1ab8ce06125e0dc22074a62dfb21c3d4259
mortif3767 <mortif3767@outlook.com>
2017-09-18 19:48:31 +0800

    test

--- manage.py
+++ manage.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 from app import create_app,db
 from app.models import User,Role,Post
 from flask_script import Manager,Shell
 from flask_migrate import Migrate,MigrateCommand
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
 
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app,db)
 
 def make_shell_context():
-	return dict(app=app,db=db,User=User,Role=Role,Post=Post)
+    return dict(app=app,db=db,User=User,Role=Role,Post=Post)
 manager.add_command("shell",Shell(make_context=make_shell_context))
 manager.add_command('db',MigrateCommand)
 
 @manager.command
-def test():
-	'''run the unit test'''
-	import unittest
-	tests=unittest.TestLoader().discover('tests')
-	unittest.TextTestRunner(verbosity=2).run(tests)
+def test(coverage=False):
+    '''run the unit test'''
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv) #如果收到coverage选项值后，脚本重启
+    import unittest
+    tests=unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 if __name__=='__main__':
-	manager.run()
+    manager.run()
